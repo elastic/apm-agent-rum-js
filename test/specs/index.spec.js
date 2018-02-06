5014709e30267845d583152af8f3642d55e85c54
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2018-02-06 13:31:17 +0100

    chore: upgrade to elastic-apm-js-core@0.2
    * https://github.com/elastic/apm-agent-js-core/blob/master/CHANGELOG.md#020-2018-02-06
    * fix tests to adopt queue model

--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -5,14 +5,17 @@ describe('index', function () {
   it('should init ApmBase', function (done) {
     apmBase.init({
       serverUrl: 'http://localhost:8200',
-      serviceName: 'apm-agent-js-base-test'
+      serviceName: 'apm-agent-js-base-test',
+      flushInterval: 100
     })
+    var apmServer = apmBase.serviceFactory.getService('ApmServer')
     if (window.globalConfigs && window.globalConfigs.useMocks) {
-      var apmServer = apmBase.serviceFactory.getService('ApmServer')
       apmServer._makeHttpRequest = function () {
         return Promise.resolve()
       }
     }
+
+    spyOn(apmServer, 'sendErrors').and.callThrough()
 
     apmBase.setUserContext({usertest: 'usertest',id: 'userId',username: 'username',email: 'email'})
     apmBase.setCustomContext({testContext: 'testContext'})
@@ -20,13 +23,20 @@ describe('index', function () {
     try {
       throw new Error('ApmBase test error')
     } catch(error) {
-      var promise = apmBase.captureError(error)
+      apmBase.captureError(error)
+      expect(apmServer.sendErrors).not.toHaveBeenCalled()
       if (apmCore.utils.isPlatformSupported()) {
-        promise.then(function () {
-          done()
-        })
+        expect(apmServer.errorQueue.items.length).toBe(1)
+        setTimeout(() => {
+          expect(apmServer.sendErrors).toHaveBeenCalled()
+          var callData = apmServer.sendErrors.calls.mostRecent()
+          callData.returnValue.then(() => {
+            done()
+          }, (reason) => {
+            fail('Failed sending error:', reason)
+          })
+        }, 200)
       } else {
-        expect(promise).toBeUndefined()
         done()
       }
     }
