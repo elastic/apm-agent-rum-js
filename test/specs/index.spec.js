2501fab31bbb6a3d97b3d63d87392d544c668ae1
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2018-02-08 15:18:12 +0100

    chore: improve index.spec test

--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -3,11 +3,6 @@ var apmCore = require('elastic-apm-js-core')
 
 describe('index', function () {
   it('should init ApmBase', function (done) {
-    apmBase.init({
-      serverUrl: 'http://localhost:8200',
-      serviceName: 'apm-agent-js-base-test',
-      flushInterval: 100
-    })
     var apmServer = apmBase.serviceFactory.getService('ApmServer')
     if (window.globalConfigs && window.globalConfigs.useMocks) {
       apmServer._makeHttpRequest = function () {
@@ -16,6 +11,26 @@ describe('index', function () {
     }
 
     spyOn(apmServer, 'sendErrors').and.callThrough()
+    spyOn(apmServer, '_postJson').and.callThrough()
+
+    try {
+      throw new Error('ApmBase test error')
+    } catch(error) {
+      apmBase.captureError(error)
+      if (apmCore.utils.isPlatformSupported()) {
+        expect(apmServer.errorQueue.items.length).toBe(1)
+        apmServer.errorQueue.flush()
+        expect(apmServer.errorQueue.items.length).toBe(0)
+        expect(apmServer.sendErrors).toHaveBeenCalled()
+        expect(apmServer._postJson).not.toHaveBeenCalled()
+        apmServer.sendErrors.calls.reset()
+      }
+    }
+    apmBase.init({
+      serverUrl: 'http://localhost:8200',
+      serviceName: 'apm-agent-js-base-test',
+      flushInterval: 100
+    })
 
     apmBase.setUserContext({usertest: 'usertest',id: 'userId',username: 'username',email: 'email'})
     apmBase.setCustomContext({testContext: 'testContext'})
