7615b21872024d7624057a20a7e31e4b81b3e35f
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2018-02-19 14:45:25 +0100

    chore: temporarily set firefox version to 44 due to apm-server#676 issue

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,9 @@ var karmaUtils = require('elastic-apm-js-core/dev-utils/karma.js')
 var testUtils = require('elastic-apm-js-core/dev-utils/test.js')
 
 module.exports = function (config) {
+  // temporarily set firefox version to 44 due to apm-server#676 issue
+  karmaUtils.baseConfig.customLaunchers.SL_FIREFOX.version = '44'
+
   config.set(karmaUtils.baseConfig)
   var testConfig = require('./test.config')
   var customeConfig = {
