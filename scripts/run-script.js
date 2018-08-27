c1ae19bc2f334c3fe389c0b03e099a11f268be36
Hamid <h.jahtalab@gmail.com>
2018-08-27 16:30:56 +0200

    chore: fix sourcemap upload url

--- scripts/run-script.js
+++ scripts/run-script.js
@@ -105,7 +105,7 @@ var scripts = {
         if (!testConfig.useMocks) {
           console.log('Uploading sourcemaps')
           var request = require('request')
-          // curl http://localhost:8200/v1/client-side/sourcemaps -X POST -F sourcemap=@app.e2e-bundle.js.map -F service_version=0.0.1 -F bundle_filepath="/test/e2e/general-usecase/app.e2e-bundle.js" -F service_name="apm-agent-js-base-test-e2e-general-usecase"
+          // curl http://localhost:8200/v1/rum/sourcemaps -X POST -F sourcemap=@app.e2e-bundle.js.map -F service_version=0.0.1 -F bundle_filepath="/test/e2e/general-usecase/app.e2e-bundle.js" -F service_name="apm-agent-js-base-test-e2e-general-usecase"
           var filepath = path.join(basePath, 'general-usecase/app.e2e-bundle.min.js.map')
           var formData = {
             sourcemap: fs.createReadStream(filepath),
@@ -114,7 +114,7 @@ var scripts = {
             service_name: 'apm-agent-js-base-test-e2e-general-usecase'
           }
           var req = request.post({
-            url: testConfig.serverUrl + '/v1/client-side/sourcemaps',
+            url: testConfig.serverUrl + '/v1/rum/sourcemaps',
             formData: formData
           },
             function (err, resp, body) {
