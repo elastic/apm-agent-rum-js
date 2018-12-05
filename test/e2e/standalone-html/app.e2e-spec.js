f5cd9b14fb67d05b9eb23ffdccecfbf00d2e6a16
Hamid <jahtalab@users.noreply.github.com>
2018-12-05 15:18:43 +0100

    chore: update js-core to v2.1.1 (#113)
    add standalone-html test

---
+++ test/e2e/standalone-html/app.e2e-spec.js
@@ -0,0 +1,15 @@
+var utils = require('elastic-apm-js-core/dev-utils/webdriver')
+
+describe('standalone-html', function () {
+  it('should run the usecase', function () {
+    browser.timeouts('script', 30000)
+    browser.url('/test/e2e/standalone-html/index.html')
+
+    browser.waitUntil(function () {
+      return browser.getText('#test-element') === 'Passed'
+    }, 10000, 'expected element #test-element')
+
+
+    return utils.allowSomeBrowserErrors(['timeout test error with a secret'])
+  })
+})
