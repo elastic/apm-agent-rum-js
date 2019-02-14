6828672d313647287fe41d6fcf8b938947759d00
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2019-02-14 12:44:11 +0100

    feat(rum-core): measure all resource entries in page load (#173)
    * feat(rum-core): measure all resource entries in page load
    
    * address review comments

--- packages/rum-core/src/performance-monitoring/capture-hard-navigation.js
+++ packages/rum-core/src/performance-monitoring/capture-hard-navigation.js
@@ -25,108 +25,152 @@
 
 const Span = require('./span')
 const Url = require('../common/url')
+const {
+  RESOURCE_INITIATOR_TYPES,
+  SPAN_THRESHOLD
+} = require('../common/constants')
 
+/**
+ * Navigation Timing Spans
+ *
+ * eventPairs[0] -> start time of span
+ * eventPairs[1] -> end time of span
+ * eventPairs[2] -> name of the span
+ */
 const eventPairs = [
   ['domainLookupStart', 'domainLookupEnd', 'Domain lookup'],
   ['connectStart', 'connectEnd', 'Making a connection to the server'],
   ['requestStart', 'responseEnd', 'Requesting and receiving the document'],
-  ['domLoading', 'domInteractive', 'Parsing the document, executing sync. scripts'],
-  ['domContentLoadedEventStart', 'domContentLoadedEventEnd', 'Fire "DOMContentLoaded" event'],
+  [
+    'domLoading',
+    'domInteractive',
+    'Parsing the document, executing sync. scripts'
+  ],
+  [
+    'domContentLoadedEventStart',
+    'domContentLoadedEventEnd',
+    'Fire "DOMContentLoaded" event'
+  ],
   ['loadEventStart', 'loadEventEnd', 'Fire "load" event']
 ]
-const spanThreshold = 5 * 60 * 1000
 
 function isValidSpan (transaction, span) {
-  var d = span.duration()
+  const duration = span.duration()
   return (
-    d < spanThreshold && d > 0 && span._start <= transaction._end && span._end <= transaction._end
+    duration < SPAN_THRESHOLD &&
+    duration > 0 &&
+    span._start <= transaction._end &&
+    span._end <= transaction._end
+  )
+}
+
+function isValidPerformanceTiming (start, end, baseTime = 0) {
+  return (
+    typeof start === 'number' &&
+    typeof end === 'number' &&
+    start >= baseTime &&
+    end > start &&
+    end - start < SPAN_THRESHOLD &&
+    start - baseTime < SPAN_THRESHOLD &&
+    end - baseTime < SPAN_THRESHOLD
   )
 }
 
 function createNavigationTimingSpans (timings, baseTime) {
-  var spans = []
-  for (var i = 0; i < eventPairs.length; i++) {
-    var start = timings[eventPairs[i][0]]
-    var end = timings[eventPairs[i][1]]
-    if (
-      baseTime &&
-      start &&
-      end &&
-      end > start &&
-      start >= baseTime &&
-      end - start < spanThreshold &&
-      start - baseTime < spanThreshold &&
-      end - baseTime < spanThreshold
-    ) {
-      var span = new Span(eventPairs[i][2], 'hard-navigation.browser-timing')
-      if (eventPairs[i][0] === 'requestStart') {
-        span.pageResponse = true
-      }
-      span._start = start - baseTime
-      span.ended = true
-      span._end = end - baseTime
-      spans.push(span)
+  const spans = []
+  for (let i = 0; i < eventPairs.length; i++) {
+    const start = timings[eventPairs[i][0]]
+    const end = timings[eventPairs[i][1]]
+
+    if (!isValidPerformanceTiming(start, end, baseTime)) {
+      continue
     }
+    const span = new Span(eventPairs[i][2], 'hard-navigation.browser-timing')
+    if (eventPairs[i][0] === 'requestStart') {
+      span.pageResponse = true
+    }
+    span._start = start - baseTime
+    span.ended = true
+    span._end = end - baseTime
+    spans.push(span)
   }
   return spans
 }
 
+function createResourceTimingSpan (name, initiatorType, start, end) {
+  let kind = 'resource'
+  if (initiatorType) {
+    kind += '.' + initiatorType
+  }
+
+  const parsedUrl = new Url(name)
+  const spanName = parsedUrl.origin + parsedUrl.path
+  const span = new Span(spanName || name, kind)
+  span.addContext({
+    http: {
+      url: name
+    }
+  })
+  span._start = start
+  span.ended = true
+  span._end = end
+  return span
+}
+
 function createResourceTimingSpans (entries, filterUrls) {
-  var spans = []
-  for (var i = 0; i < entries.length; i++) {
-    var entry = entries[i]
-    var { initiatorType, name, startTime, responseEnd } = entry
+  const spans = []
+  for (let i = 0; i < entries.length; i++) {
+    let { initiatorType, name, startTime, responseEnd } = entries[i]
     /**
      * Skipping the timing information of API calls because of auto patching XHR and Fetch
      */
-    if (initiatorType === 'xmlhttprequest' || initiatorType === 'fetch' || !entry.name) {
-      continue
-    } else if (
-      initiatorType !== 'css' &&
-      initiatorType !== 'img' &&
-      initiatorType !== 'script' &&
-      initiatorType !== 'link'
+    if (
+      initiatorType === 'xmlhttprequest' ||
+      initiatorType === 'fetch' ||
+      !name
     ) {
-      // is ajax request? test for css/img before the expensive operation
-      var foundAjaxReq = false
-      for (var j = 0; j < filterUrls.length; j++) {
-        var idx = name.lastIndexOf(filterUrls[j])
+      continue
+    } else if (!isValidPerformanceTiming(startTime, responseEnd)) {
+      continue
+    }
+
+    /**
+     * Create spans for all known resource initiator types
+     */
+    if (RESOURCE_INITIATOR_TYPES.indexOf(initiatorType) !== -1) {
+      spans.push(
+        createResourceTimingSpan(name, initiatorType, startTime, responseEnd)
+      )
+    } else {
+      /**
+       * Since IE does not support initiatorType in Resource timing entry,
+       * We have to manually filter the API calls from creating duplicate Spans
+       *
+       * Skip span creation if initiatorType is other than known types specified as part of RESOURCE_INITIATOR_TYPES
+       * The reason being, there are other types like embed, video, audio, navigation etc
+       *
+       * Check the below webplatform test to know more
+       * https://github.com/web-platform-tests/wpt/blob/b0020d5df18998609b38786878f7a0b92cc680aa/resource-timing/resource_initiator_types.html#L93
+       */
+      if (initiatorType != null) {
+        continue
+      }
+
+      let foundAjaxReq = false
+      for (let j = 0; j < filterUrls.length; j++) {
+        const idx = name.lastIndexOf(filterUrls[j])
         if (idx > -1 && idx === name.length - filterUrls[j].length) {
           foundAjaxReq = true
           break
         }
       }
-      if (foundAjaxReq) {
-        continue
-      }
-    } else {
-      var kind = 'resource'
-      if (initiatorType) {
-        kind += '.' + initiatorType
-      }
-      var start = startTime
-      var end = responseEnd
-      if (
-        typeof start === 'number' &&
-        typeof end === 'number' &&
-        start >= 0 &&
-        end > start &&
-        end - start < spanThreshold &&
-        start < spanThreshold &&
-        end < spanThreshold
-      ) {
-        var parsedUrl = new Url(name)
-        var spanName = parsedUrl.origin + parsedUrl.path
-        var span = new Span(spanName || name, kind)
-        span.addContext({
-          http: {
-            url: name
-          }
-        })
-        span._start = start
-        span.ended = true
-        span._end = end
-        spans.push(span)
+      /**
+       * Create span if its not an ajax request
+       */
+      if (!foundAjaxReq) {
+        spans.push(
+          createResourceTimingSpan(name, initiatorType, startTime, responseEnd)
+        )
       }
     }
   }
@@ -134,14 +178,16 @@ function createResourceTimingSpans (entries, filterUrls) {
 }
 
 function captureHardNavigation (transaction) {
-  if (transaction.isHardNavigation && window.performance && window.performance.timing) {
-    var timings = window.performance.timing
-    var baseTime = timings.fetchStart
+  const perf = window.performance
+  if (transaction.isHardNavigation && perf && perf.timing) {
+    const timings = perf.timing
     // must be zero otherwise the calculated relative _start time would be wrong
     transaction._start = 0
     transaction.type = 'page-load'
 
-    createNavigationTimingSpans(timings, baseTime).forEach(function (span) {
+    createNavigationTimingSpans(timings, timings.fetchStart).forEach(function (
+      span
+    ) {
       if (isValidSpan(transaction, span)) {
         span.traceId = transaction.traceId
         span.sampled = transaction.sampled
@@ -152,17 +198,18 @@ function captureHardNavigation (transaction) {
       }
     })
 
-    if (window.performance.getEntriesByType) {
-      var entries = window.performance.getEntriesByType('resource')
+    if (typeof perf.getEntriesByType === 'function') {
+      const entries = perf.getEntriesByType('resource')
 
-      var ajaxUrls = transaction.spans
-        .filter(function (span) {
-          return span.type === 'external' && span.subType === 'http'
-        })
-        .map(function (span) {
-          return span.name.split(' ')[1]
-        })
+      const ajaxUrls = []
+      for (let i = 0; i < transaction.spans; i++) {
+        const span = transaction.spans[i]
 
+        if (span.type === 'external' && span.subType === 'http') {
+          continue
+        }
+        ajaxUrls.push(span.name.split(' ')[1])
+      }
       createResourceTimingSpans(entries, ajaxUrls).forEach(function (span) {
         if (isValidSpan(transaction, span)) {
           transaction.spans.push(span)
