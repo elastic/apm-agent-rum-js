d502773f4a27961d3ce77e90606963b611bf698f
Hamid <jahtalab@users.noreply.github.com>
2019-03-21 11:21:56 +0100

    chore: use 7.0.0-beta1 for tests (#222)
    *  chore: use 7.0.0-beta1 stack version for testing
    
    * docs: update source map endpoint

--- packages/rum/test/node/upload-sourcemaps.node-spec.js
+++ packages/rum/test/node/upload-sourcemaps.node-spec.js
@@ -35,7 +35,7 @@ const { serverUrl } = getTestEnvironmentVariables()
 
 describe('Sourcemaps', function() {
   it('should upload sourcemaps', function(done) {
-    // curl http://localhost:8200/v1/rum/sourcemaps -X POST -F sourcemap=@app.e2e-bundle.js.map -F service_version=0.0.1 -F bundle_filepath="/test/e2e/general-usecase/app.e2e-bundle.js" -F service_name="apm-agent-rum-test-e2e-general-usecase"
+    // curl http://localhost:8200/assets/v1/sourcemaps -X POST -F sourcemap=@app.e2e-bundle.js.map -F service_version=0.0.1 -F bundle_filepath="/test/e2e/general-usecase/app.e2e-bundle.js" -F service_name="apm-agent-rum-test-e2e-general-usecase"
     var filepath = path.join(
       basePath,
       'general-usecase/app.e2e-bundle.min.js.map'
@@ -50,7 +50,7 @@ describe('Sourcemaps', function() {
 
     request.post(
       {
-        url: serverUrl + '/v1/rum/sourcemaps',
+        url: serverUrl + '/assets/v1/sourcemaps',
         formData
       },
       function(err, resp, body) {
