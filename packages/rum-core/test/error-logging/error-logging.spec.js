ef61121eaff86a30f8b48a598379c6d8d9fadf53
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2019-06-12 11:42:41 +0200

    feat(rum-core): use error event instead of global onerror method (#281)
    * feat(rum-core): use error event instead of global onerror method
    
    * test with patched listener as per review

--- packages/rum-core/test/error-logging/error-logging.spec.js
+++ packages/rum-core/test/error-logging/error-logging.spec.js
@@ -174,9 +174,10 @@ describe('ErrorLogging', function() {
       .then(() => done())
   })
 
-  it('should install onerror and accept ErrorEvents', function(done) {
+  it('should install global listener for error and accept ErrorEvents', function(done) {
     var count = 0
-    var numberOfErrors = 7
+    var numberOfErrors = 4
+    const original = window.addEventListener
     spyOn(apmServer, 'sendErrors').and.callFake(function(errors) {
       expect(errors.length).toBe(numberOfErrors)
       var error = errors[0]
@@ -184,34 +185,44 @@ describe('ErrorLogging', function() {
 
       count = count + errors.length
       if (count === numberOfErrors) {
+        window.addEventListener = original
         done()
       }
     })
 
-    window.onerror = null
+    const addedListenerTypes = []
+    let listener = (window.addEventListener = function(type, event) {
+      addedListenerTypes.push(type)
+      errorLogging.logErrorEvent(event)
+    })
     errorLogging.registerGlobalEventListener()
+    expect(addedListenerTypes).toContain('error')
 
-    expect(typeof window.onerror).toBe('function')
-    var apmOnError = window.onerror
+    const filename = 'filename'
+    const lineno = 1
+    const colno = 2
 
     try {
       throw new Error(testErrorMessage)
     } catch (error) {
-      apmOnError(testErrorMessage, 'filename', 1, 2, error)
+      listener('error', {
+        message: testErrorMessage,
+        filename,
+        lineno,
+        colno,
+        error
+      })
     }
-
-    apmOnError(testErrorMessage, 'filename', 1, 2, undefined)
-    apmOnError(testErrorMessage, 'filename', 1, 2, testErrorMessage) // throw "test";
-    apmOnError(testErrorMessage, undefined, undefined, undefined, undefined)
-    apmOnError('Test:' + testErrorMessage, 'filename', 1, 2, undefined)
-    apmOnError(
-      'Script error.' + testErrorMessage,
-      undefined,
-      undefined,
-      undefined,
-      undefined
-    )
-    apmOnError(createErrorEvent(testErrorMessage))
+    listener('error', {
+      message: testErrorMessage,
+      filename,
+      lineno,
+      colno
+    })
+    listener('error', {
+      message: 'Script error.' + testErrorMessage
+    })
+    listener('error', createErrorEvent(testErrorMessage))
   })
 
   it('should handle edge cases', function(done) {
