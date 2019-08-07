c726776d3469ac94940dbd3c8e779bec10b378a0
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2019-08-07 18:58:01 +0200

    fix package.json and notice

--- packages/rum/test/e2e/index.js
+++ packages/rum/test/e2e/index.js
@@ -23,6 +23,7 @@
  *
  */
 
+import { polyfill } from 'es6-promise'
 import { apmBase } from '../../src'
 import { getGlobalConfig } from '../../../../dev-utils/test-config'
 import ApmServerMock from '../../../rum-core/test/utils/apm-server-mock'
@@ -30,6 +31,12 @@ import ApmServerMock from '../../../rum-core/test/utils/apm-server-mock'
 const globalConfig = getGlobalConfig()
 
 function createApmBase(config) {
+  /**
+   * Polyfill the global promise since webdriver
+   * functions uses promise based API
+   * ex: browser.execute, browser.executeAsy
+   */
+  polyfill()
   console.log('E2E Global Configs', JSON.stringify(globalConfig, null, 2))
   const apmServer = apmBase.serviceFactory.getService('ApmServer')
   const { serverUrl } = globalConfig.agentConfig
