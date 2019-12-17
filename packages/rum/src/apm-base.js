77ccd4be10a82b19ab677616bce3468167ded780
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2019-12-17 16:17:52 +0100

    chore(rum): clean up public API and use API methods

--- packages/rum/src/apm-base.js
+++ packages/rum/src/apm-base.js
@@ -123,15 +123,11 @@ class ApmBase {
   }
 
   _sendPageLoadMetrics() {
-    const transactionService = this.serviceFactory.getService(
-      'TransactionService'
-    )
-
     /**
      * Name of the transaction is set in transaction service to
      * avoid duplicating the logic at multiple places
      */
-    const tr = transactionService.startTransaction(undefined, PAGE_LOAD, {
+    const tr = this.startTransaction(undefined, PAGE_LOAD, {
       managed: true,
       canReuse: true
     })
@@ -260,15 +256,6 @@ class ApmBase {
         'TransactionService'
       )
       return transactionService.getCurrentTransaction()
-    }
-  }
-
-  getTransactionService() {
-    if (this.isEnabled()) {
-      var transactionService = this.serviceFactory.getService(
-        'TransactionService'
-      )
-      return transactionService
     }
   }
 
