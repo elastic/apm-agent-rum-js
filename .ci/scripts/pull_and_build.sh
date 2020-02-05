95b712bc185e67f82945f34afa59cf8d15dfa46b
Victor Martinez <victormartinezrubio@gmail.com>
2020-02-05 19:43:19 +0000

    ci(jenkins): less verbose console output (#605)
    * ci(jenkins): less verbose console output
    
    * docker-compose build  --quiet is not supported in the current version in the CI

--- .ci/scripts/pull_and_build.sh
+++ .ci/scripts/pull_and_build.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-docker-compose -f ./dev-utils/docker-compose.yml pull --ignore-pull-failures
-docker-compose -f ./dev-utils/docker-compose.yml build --force-rm
+docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO pull --quiet --ignore-pull-failures
+docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO build --force-rm >docker-compose.log 2>docker-compose.err
+if [ $? -gt 0 ] ; then
+  echo "Docker compose failed, see the below log output"
+  cat docker-compose.log && rm docker-compose.log
+  cat docker-compose.err && rm docker-compose.err
+  exit 1
+else
+  rm docker-compose.log docker-compose.err
+fi
--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 USER_ID="$(id -u):$(id -g)" \
-docker-compose -f ./dev-utils/docker-compose.yml up \
+docker-compose -f ./dev-utils/docker-compose.yml \
+  --log-level INFO \
+  up \
+  --quiet-pull \
   --abort-on-container-exit \
   --exit-code-from node-puppeteer \
   --remove-orphans \
