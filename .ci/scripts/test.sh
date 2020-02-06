c675e5e86c4354e91a6e418d9cd67aa8b1705688
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-02-06 14:47:38 +0100

    fix: generate junit

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -2,12 +2,11 @@
 set -xueo pipefail
 
 USER_ID="$(id -u):$(id -g)" \
-docker-compose -f ./dev-utils/docker-compose.yml \
+docker-compose \
+  -f ./dev-utils/docker-compose.yml \
   --log-level INFO \
   up \
   --quiet-pull \
-  --no-ansi \
-  --abort-on-container-exit \
   --exit-code-from node-puppeteer \
   --abort-on-container-exit \
   --remove-orphans \
