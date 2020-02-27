ce8e6026766485a2efd1f452976f821ae4449d21
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-02-27 11:28:10 +0000

    chrome is not required to be installed this way as the playwritch is the one

--- packages/rum/test/benchmarks/config.js
+++ packages/rum/test/benchmarks/config.js
@@ -28,7 +28,7 @@ module.exports = {
   scenarios: ['basic', 'heavy'],
   runs: 3,
   noOfImages: 30,
-  browserTypes: ['chromium', 'firefox', 'webkit'],
+  browserTypes: ['chromium'],
   port,
   chrome: {
     /**
