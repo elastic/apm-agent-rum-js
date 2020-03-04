7671ac2a2edc6ca31ae4a082ee5da3da0e54a38e
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2020-03-04 14:18:29 +0100

    chore(benchmark): run only on chromium and exit process (#676)
    * chore(benchmark): run only on chromium and exit process
    
    * install playwright and move to node 12

--- packages/rum/test/benchmarks/config.js
+++ packages/rum/test/benchmarks/config.js
@@ -28,7 +28,7 @@ module.exports = {
   scenarios: ['basic', 'heavy'],
   runs: 3,
   noOfImages: 30,
-  browserTypes: ['chromium', 'firefox', 'webkit'],
+  browserTypes: ['chromium'],
   port,
   chrome: {
     /**
@@ -42,6 +42,7 @@ module.exports = {
     memorySamplingInterval: 10,
     launchOptions: {
       headless: true,
+      dumpio: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
   }
