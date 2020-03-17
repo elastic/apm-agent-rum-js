d2c169b4eae589b793dfa6ad3d3f6741bd94e57b
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-17 11:55:53 +0000

    Revert "ci(jenkins): for debugging"
    This reverts commit 4ee0aae42ed886627a4afdc4686c81699fb84c60.

--- .ci/scripts/lint.sh
+++ .ci/scripts/lint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 npm install
 npm run lint
-echo "Commitlint ..."
 scripts/lint-commits.sh
--- commitlint.config.js
+++ commitlint.config.js
@@ -29,7 +29,6 @@ const {
 } = require('@commitlint/config-lerna-scopes')
 
 module.exports = {
-  extends: ['@commitlint/config-lerna-scopes'],
   rules: {
     'scope-enum': async ctx => [
       2,
