87c1def7472815a93c73ac2b5ac4b2ac2eba94fe
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-17 11:55:23 +0000

    Revert "ci: use flag"
    This reverts commit 5483e388854e098398134c81f4ca102b6dd9ffb4.

--- scripts/lint-commits.sh
+++ scripts/lint-commits.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 set -xeo pipefail
 
-
-FLAG="-g commitlint.config.js"
 # Run if we're not on Jenkins
 if [[ -n "${JENKINS_URL}" ]]; then
   export PATH=$(npm bin):${PATH}
   export HOME=$(pwd)
   if [[ -z "${CHANGE_ID}" ]]; then
     # If on master, just test the latest commit
-    commitlint --from="${GIT_SHA}~1" "${FLAG}"
+    commitlint --from="${GIT_SHA}~1"
   else
     # If on a branch, test all commits between this branch and master
-    commitlint --from="origin/${CHANGE_TARGET}" --to="${GIT_BASE_COMMIT}" "${FLAG}"
+    commitlint --from="origin/${CHANGE_TARGET}" --to="${GIT_BASE_COMMIT}"
 
     # Lint PR title
     if [[ -n ${CHANGE_TITLE} ]]; then
-      echo "${CHANGE_TITLE}" | commitlint "${FLAG}"
+      echo "${CHANGE_TITLE}" | commitlint
     fi
   fi
 # Run if we're not on Travis
