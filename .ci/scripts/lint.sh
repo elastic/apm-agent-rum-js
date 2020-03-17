4ee0aae42ed886627a4afdc4686c81699fb84c60
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-17 11:32:43 +0000

    ci(jenkins): for debugging

--- .ci/scripts/lint.sh
+++ .ci/scripts/lint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 npm install
 npm run lint
+echo "Commitlint ..."
 scripts/lint-commits.sh
--- commitlint.config.js
+++ commitlint.config.js
@@ -29,6 +29,7 @@ const {
 } = require('@commitlint/config-lerna-scopes')
 
 module.exports = {
+  extends: ['@commitlint/config-lerna-scopes'],
   rules: {
     'scope-enum': async ctx => [
       2,
