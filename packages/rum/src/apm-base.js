360638490febb7dcbe69c666b8639c73b4ef52d0
Hamid <h.jahtalab@gmail.com>
2020-03-18 14:31:23 +0100

    chore(release): publish
     - @elastic/apm-rum-angular@1.0.0
     - @elastic/apm-rum-core@5.0.0
     - @elastic/apm-rum-react@1.0.0
     - @elastic/apm-rum-vue@1.0.0
     - @elastic/apm-rum@5.0.0

--- packages/rum/src/apm-base.js
+++ packages/rum/src/apm-base.js
@@ -43,7 +43,7 @@ class ApmBase {
       /**
        * Set Agent version to be sent as part of metadata to the APM Server
        */
-      configService.setVersion('4.9.1')
+      configService.setVersion('5.0.0')
       this.config(config)
       const loggingService = this.serviceFactory.getService('LoggingService')
       /**
