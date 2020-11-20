3daf2553b9d6484f686364ebf53a883183db9f0f
Manuel de la Peña <mdelapenya@gmail.com>
2020-11-20 15:39:05 +0100

    chore(jenkins): cache images in packer

---
+++ .ci/packer_cache.sh
@@ -0,0 +1,14 @@
+# shellcheck disable=SC1091
+source /usr/local/bin/bash_standard_lib.sh
+
+readonly NODEJS_VERSION="12"
+
+DOCKER_IMAGES="docker.elastic.co/observability-ci/node-playwright:${NODEJS_VERSION}
+docker.elastic.co/observability-ci/node-puppeteer:${NODEJS_VERSION}"
+
+if [ -x "$(command -v docker)" ]; then
+  for di in ${DOCKER_IMAGES}
+  do
+  (retry 2 docker pull "${di}") || echo "Error pulling ${di} Docker image, we continue"
+  done
+fi
