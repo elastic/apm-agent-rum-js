c2cbc42537e9307abb3ee1d5a4f13a7e6f72ea65
Manuel de la Peña <social.mdelapenya@gmail.com>
2020-11-24 13:20:20 +0100

    chore: pass NodeJS variable properly
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>

--- .ci/scripts/pull_and_build.sh
+++ .ci/scripts/pull_and_build.sh
@@ -6,7 +6,7 @@ docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO pull --quiet -
 # We are building the images here even though the Docker images are already cached in Packer.
 # This is because there could be changes in the PR affecting the files copied to the Docker image,
 # which we want to test in the current build. 
-docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO build >docker-compose.log 2>docker-compose.err
+NODEJS_VERSION=${NODEJS_VERSION} docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO build >docker-compose.log 2>docker-compose.err
 if [ $? -gt 0 ] ; then
   echo "Docker compose failed, see the below log output"
   cat docker-compose.log && rm docker-compose.log
