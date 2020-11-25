72495e4835557b31daf0e16b4c0e8d86c635d7f5
Manuel de la Peña <mdelapenya@gmail.com>
2020-11-25 17:20:13 +0100

    chore(jenkins): do not build test images (#935)
    * chore: add a new scope for tsts in commitlint
    
    * chore: do not build test images
    
    We prefer consuming them
    
    * chore(jenkins): document how to build test images
    
    * chore(jenkins): update script
    
    * chore(jenkins): rename script
    
    * Revert "chore(jenkins): rename script"
    
    This reverts commit 053d073f280ca0e56361e9727d35f410bf501730.
    
    * Revert "chore(jenkins): update script"
    
    This reverts commit 5dbb768b4f78f362e72642873c157556cc9ea903.
    
    * chore(jenkins): do not remove cached images when building
    
    * Revert "chore: do not build test images"
    
    This reverts commit b3e0fad30fca1bfaa85a475c14f83287e3e62838.
    
    * chore(jenkins): cache images in packer
    
    * Revert "chore: add a new scope for tsts in commitlint"
    
    This reverts commit 2f7f8226fee6961afc7bea078fc97bc7e975bb28.
    
    * chore: define node version for tests only once
    
    * chore: simplify docker build, as we only support one NodeJS version in tests
    
    * chore: always build images in Packer
    
    We want to build them there, creating a baseline for the current PR to be
    compared against it. Docker will reuse the not modified layers, speeding
    up the build
    
    * chore: add EOF to node-version file
    
    * chore: reorder pull and build sentences
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>
    
    * chore: pass NodeJS variable properly
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>

--- .ci/scripts/pull_and_build.sh
+++ .ci/scripts/pull_and_build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+export NODEJS_VERSION=$(cat ./dev-utils/.node-version)
+
 docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO pull --quiet --ignore-pull-failures
-docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO build --force-rm >docker-compose.log 2>docker-compose.err
+
+# We are building the images here even though the Docker images are already cached in Packer.
+# This is because there could be changes in the PR affecting the files copied to the Docker image,
+# which we want to test in the current build. 
+NODEJS_VERSION=${NODEJS_VERSION} docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO build >docker-compose.log 2>docker-compose.err
 if [ $? -gt 0 ] ; then
   echo "Docker compose failed, see the below log output"
   cat docker-compose.log && rm docker-compose.log
