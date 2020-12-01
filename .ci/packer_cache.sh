709bc36e88c0b871bb5f3ff9f3daa01822a08b85
Victor Martinez <victormartinezrubio@gmail.com>
2020-12-01 16:51:23 +0000

    chore(jenkins): fix running issues with the packer-cache (#941)

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -9,8 +9,8 @@ puppeteer"
 if [ -x "$(command -v docker)" ]; then
   for flavour in ${FLAVOURS}
   do
-  local di="docker.elastic.co/observability-ci/node-${flavour}:${NODEJS_VERSION}"
+  di="docker.elastic.co/observability-ci/node-${flavour}:${NODEJS_VERSION}"
   (retry 2 docker pull "${di}") || echo "Error pulling ${di} Docker image, we continue"
-  docker build --build-arg NODEJS_VERSION=${NODEJS_VERSION} -t ${di} ./.ci/docker/node-${flavour}
+  docker build --build-arg NODEJS_VERSION="${NODEJS_VERSION}" -t "${di}" .ci/docker/node-"${flavour}"  || echo "Error building ${flavour} Docker image, we continue"
   done
 fi
