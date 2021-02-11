c4b9209acff70a3d771741af65bd18dd37f5b0eb
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-02-11 15:40:36 +0000

    ci: docker compose down

--- .ci/scripts/benchmarks.sh
+++ .ci/scripts/benchmarks.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-USER_ID="$(id -u):$(id -g)" \
-NODEJS_VERSION=$(cat ./dev-utils/.node-version) \
+
+USER_ID="$(id -u):$(id -g)"
+NODEJS_VERSION=$(cat ./dev-utils/.node-version)
+
+USER_ID="${USER_ID}" \
+NODEJS_VERSION="${NODEJS_VERSION}" \
+docker-compose -f ./dev-utils/docker-compose.yml down \
+  --remove-orphans \
+  --volumes || true
+
+USER_ID="${USER_ID}" \
+NODEJS_VERSION="${NODEJS_VERSION}" \
 docker-compose -f ./dev-utils/docker-compose.yml up \
   --build \
   --abort-on-container-exit \
--- .ci/scripts/load-testing.sh
+++ .ci/scripts/load-testing.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
-USER_ID="$(id -u):$(id -g)" \
-NODEJS_VERSION=$(cat ./dev-utils/.node-version) \
-STACK_VERSION=${1:-7.10.0} \
+
+STACK_VERSION=${1:-7.10.0}
+USER_ID="$(id -u):$(id -g)"
+NODEJS_VERSION=$(cat ./dev-utils/.node-version)
+
+USER_ID="${USER_ID}" \
+NODEJS_VERSION="${NODEJS_VERSION}" \
+STACK_VERSION=${STACK_VERSION} \
+docker-compose -f ./dev-utils/docker-compose.yml down \
+  --remove-orphans \
+  --volumes || true
+
+USER_ID="${USER_ID}" \
+NODEJS_VERSION="${NODEJS_VERSION}" \
+STACK_VERSION=${STACK_VERSION} \
 docker-compose -f ./dev-utils/docker-compose.yml up \
   --build \
   --abort-on-container-exit \
