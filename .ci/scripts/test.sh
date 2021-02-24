f92c309c0a8af50ecfa7fc21b1b84498536e82f4
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-02-24 17:10:04 +0000

    ci: force default value for the stack

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -5,6 +5,7 @@ pip install docker-compose>=1.25.4
 
 USER_ID="$(id -u):$(id -g)" \
 NODEJS_VERSION=$(cat ./dev-utils/.node-version) \
+STACK_VERSION=${STACK_VERSION} \
 docker-compose \
   -f ./dev-utils/docker-compose.yml \
   --log-level INFO \
