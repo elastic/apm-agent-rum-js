32e18568c509723bf2d4d1a04de80fe1e4724a32
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-02-24 16:39:42 +0000

    ci: somehow the env variable is needed now

--- .ci/scripts/pull_and_build.sh
+++ .ci/scripts/pull_and_build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 export NODEJS_VERSION=$(cat ./dev-utils/.node-version)
+export STACK_VERSION=${STACK_VERSION:-7.10.0}
 
+STACK_VERSION=${STACK_VERSION} \
 docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO pull --quiet --ignore-pull-failures
 
 # We are building the images here even though the Docker images are already cached in Packer.
 # This is because there could be changes in the PR affecting the files copied to the Docker image,
 # which we want to test in the current build. 
-NODEJS_VERSION=${NODEJS_VERSION} docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO build >docker-compose.log 2>docker-compose.err
+NODEJS_VERSION="${NODEJS_VERSION}" \
+STACK_VERSION=${STACK_VERSION} \
+docker-compose -f ./dev-utils/docker-compose.yml --log-level INFO build >docker-compose.log 2>docker-compose.err
 if [ $? -gt 0 ] ; then
   echo "Docker compose failed, see the below log output"
   cat docker-compose.log && rm docker-compose.log
