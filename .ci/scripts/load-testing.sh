07facf83f21460cb83c1907954a91b2f55bb6fd6
Victor Martinez <victormartinezrubio@gmail.com>
2021-03-15 14:56:40 +0000

    ci: run apm-server load testing (#959)

--- .ci/scripts/load-testing.sh
+++ .ci/scripts/load-testing.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+STACK_VERSION=${1:-7.10.0}
 USER_ID="$(id -u):$(id -g)"
 NODEJS_VERSION=$(cat ./dev-utils/.node-version)
 
 USER_ID="${USER_ID}" \
 NODEJS_VERSION="${NODEJS_VERSION}" \
+STACK_VERSION=${STACK_VERSION} \
 docker-compose -f ./dev-utils/docker-compose.yml down \
   --remove-orphans \
   --volumes || true
 
 USER_ID="${USER_ID}" \
 NODEJS_VERSION="${NODEJS_VERSION}" \
+STACK_VERSION=${STACK_VERSION} \
 docker-compose -f ./dev-utils/docker-compose.yml up \
   --build \
   --abort-on-container-exit \
-  --exit-code-from node-benchmark \
+  --exit-code-from load-testing \
   --remove-orphans \
-  node-benchmark
+  load-testing
