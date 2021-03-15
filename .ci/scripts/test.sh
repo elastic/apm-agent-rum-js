9f5454a1cc85a112c81d81d4e78c9358dda2ec97
Victor Martinez <victormartinezrubio@gmail.com>
2021-03-15 14:56:40 +0000

    ci: run apm-server load testing (#959)

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-set -xueo pipefail
+set -xeo pipefail
+STACK_VERSION=${STACK_VERSION:-7.10.0}
 
 pip install docker-compose>=1.25.4
 
 USER_ID="$(id -u):$(id -g)" \
 NODEJS_VERSION=$(cat ./dev-utils/.node-version) \
+STACK_VERSION=${STACK_VERSION} \
 docker-compose \
   -f ./dev-utils/docker-compose.yml \
   --log-level INFO \
