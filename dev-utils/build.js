308f830cbc28dc64ae58674f44507f64a0833f81
Lucas Fernandes da Costa <lucas.costa@elastic.co>
2022-01-19 15:52:15 +0000

    refactor: move babel exclusion rule to its own var and reuse

--- dev-utils/build.js
+++ dev-utils/build.js
@@ -56,6 +56,11 @@ const PACKAGE_TYPES = {
   VUE: 'VUE'
 }
 
+// We must exclude `parse5` from the packages we ignore
+// because otherwise it would not be transpiled. Therefore,
+// it would break IE11.
+const babelExclusionRule = /node_modules\/(?!parse5).+/
+
 function getBabelPresetEnv(bundleType) {
   const isBrowser = [
     BROWSER_DEV,
@@ -152,10 +157,7 @@ function getBabelConfig(bundleType, packageType) {
   let options = {
     comments: false,
     babelrc: false,
-    // We must exclude `parse5` from the packages we ignore
-    // because otherwise it would not be transplied. Therefore,
-    // it would break IE11.
-    exclude: /node_modules\/(?!parse5).+/,
+    exclude: babelExclusionRule,
     presets: getBabelPresetEnv(bundleType),
     plugins: []
   }
@@ -205,10 +207,7 @@ function getCommonWebpackConfig(bundleType, packageType) {
       rules: [
         {
           test: /\.(js|jsx|ts)$/,
-          // We must exclude `parse5` from the packages we ignore
-          // because otherwise it would not be transplied. Therefore,
-          // it would break IE11.
-          exclude: /node_modules\/(?!parse5).+/,
+          exclude: babelExclusionRule,
           loader: 'babel-loader',
           options: getBabelConfig(bundleType, packageType)
         }
