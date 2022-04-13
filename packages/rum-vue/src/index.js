49bb77c71649e5f394ae4940d66e2091f2e8c729
Christian Peters <cp@gschwind-software.de>
2022-04-13 11:59:27 +0200

    feat(rum-vue): vue 3 script setup syntax support

--- packages/rum-vue/src/index.js
+++ packages/rum-vue/src/index.js
@@ -56,7 +56,10 @@ export const ApmVuePlugin = {
     /**
      * Provide the APM instance via $apm to be accessed in all Vue Components
      */
-    if (majorVersion >= 3) app.config.globalProperties.$apm = apm
+    if (majorVersion >= 3) {
+      app.config.globalProperties.$apm = apm
+      app.provide("$apm", apm)
+    }
     /**
      * Backward compatibility with Vue 2
      */ else app.prototype.$apm = apm
