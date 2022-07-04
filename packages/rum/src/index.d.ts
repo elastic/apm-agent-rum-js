cfaa327ba3715c90750157eab66149be8437d2a8
RW <rene@gofiber.io>
2022-07-04 19:14:22 +0200

    feat(rum-core): add apmRequest config typescript type (#1254)

--- packages/rum/src/index.d.ts
+++ packages/rum/src/index.d.ts
@@ -101,6 +101,15 @@ declare module '@elastic/apm-rum' {
     eventsLimit?: number
     queueLimit?: number
     sendCredentials?: boolean
+    apmRequest?: (
+      requestParams: {
+        xhr: XMLHttpRequest
+        url: string
+        method: string
+        payload?: string
+        headers?: Record<string, string>
+      },
+    ) => boolean
   }
 
   type Init = (options?: AgentConfigOptions) => ApmBase
