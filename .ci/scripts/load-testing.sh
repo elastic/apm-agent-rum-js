7dc0148c9cd55cc9b29920e841073d5cf8a23e77
David Luna <luna.bistuer@gmail.com>
2026-08-03 16:27:09 +0200

    chore: remove unused env var in docke file

--- .ci/scripts/load-testing.sh
+++ .ci/scripts/load-testing.sh
@@ -4,7 +4,6 @@ STACK_VERSION=${1:-8.19.6}
 USER_ID="$(id -u):$(id -g)"
 
 USER_ID="${USER_ID}" \
-NODEJS_VERSION="${NODEJS_VERSION}" \
 STACK_VERSION=${STACK_VERSION} \
 docker-compose -f ./dev-utils/docker-compose.yml down \
   --remove-orphans \
