0e484d497c91537f0dd32e0bc58e0cd17dcfb10b
David Luna <luna.bistuer@gmail.com>
2026-08-03 15:39:53 +0200

    chore: ise node 20 for load testing

--- .ci/scripts/load-testing.sh
+++ .ci/scripts/load-testing.sh
@@ -2,10 +2,6 @@
 
 STACK_VERSION=${1:-8.19.6}
 USER_ID="$(id -u):$(id -g)"
-# The migration of this to Node.js 18 will wait. To update playwright (and browsers)
-# to make it work in Node.js 18 and latest version in ubuntu (or another OS)
-# will require us to invest a certain amount of time
-NODEJS_VERSION=14
 
 USER_ID="${USER_ID}" \
 NODEJS_VERSION="${NODEJS_VERSION}" \
@@ -15,7 +11,6 @@ docker-compose -f ./dev-utils/docker-compose.yml down \
   --volumes || true
 
 USER_ID="${USER_ID}" \
-NODEJS_VERSION="${NODEJS_VERSION}" \
 STACK_VERSION=${STACK_VERSION} \
 docker-compose -f ./dev-utils/docker-compose.yml up \
   --build \
