9ee68b87da486738f5e35b735a61835571eaa72a
David Luna <luna.bistuer@gmail.com>
2026-08-03 18:11:37 +0200

    chore: change env vars

--- .ci/scripts/load-testing.sh
+++ .ci/scripts/load-testing.sh
@@ -3,13 +3,17 @@
 STACK_VERSION=${1:-8.19.6}
 USER_ID="$(id -u):$(id -g)"
 
-USER_ID="${USER_ID}" \
+# Ensure local bin is in PATH, needed for updated docker-compose, see https://github.com/elastic/observability-robots/issues/2960
+export PATH="$HOME/.local/bin:$PATH"
+
+
+# USER_ID="${USER_ID}" \
 STACK_VERSION=${STACK_VERSION} \
 docker-compose -f ./dev-utils/docker-compose.yml down \
   --remove-orphans \
   --volumes || true
 
-USER_ID="${USER_ID}" \
+# USER_ID="${USER_ID}" \
 STACK_VERSION=${STACK_VERSION} \
 docker-compose -f ./dev-utils/docker-compose.yml up \
   --build \
